46446c399b39534da17b4088c1b69eadc73c4531
Rajasekar0104 <118898832+Rajasekar0104@users.noreply.github.com>
2026-01-03 20:34:10 +0530

    Create 06_string_methods.py

---
+++ 02_strings/06_string_methods.py
@@ -0,0 +1,10 @@
+# Strings come with many built-in methods
+
+t = "VAaleo"
+
+print(t.upper())       # Convert to uppercase
+print(t.lower())       # Convert to lowercase
+print(t.capitalize())  # Capitalize first letter
+print(t.casefold())    # Aggressive lowercase
+print(t.count("a"))    # Count occurrences
+print(t.find("e"))     # Find position
