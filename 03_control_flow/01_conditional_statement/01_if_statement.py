764098d09f9b8339a47cdb8612412ad89629832a
Rajasekar0104 <118898832+Rajasekar0104@users.noreply.github.com>
2026-01-26 19:44:09 +0530

    Update 01_if_statement.py

--- 03_control_flow/01_conditional_statement/01_if_statement.py
+++ 03_control_flow/01_conditional_statement/01_if_statement.py
@@ -1,1 +1,7 @@
+# IF is used when an action is required only if a condition is true
 
+interface_status = "up"
+
+# Check if interface is operational
+if interface_status == "up":
+    print("✅ Interface is UP – traffic can flow")
